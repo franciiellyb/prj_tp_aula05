570aca7218641eb4717bc14be1b46829841227b8
Francielly Santos <francielly2403@gmail.com>
2026-03-12 14:52:03 -0300

    Create Ex_01.java

---
+++ Ex_01.java
@@ -0,0 +1,16 @@
+package prj_TpAula05;
+import java.util.Scanner;
+public class Ex_01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro: ");
+		int num = sc.nextInt();
+		
+		String result = (num>=0) ? "NAO NEGATIVO" : "NEGATIVO";
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
