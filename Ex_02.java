8b279fce1f275470e63466156f7c25608c9ab797
Francielly Santos <francielly2403@gmail.com>
2026-03-12 14:52:36 -0300

    Add Ex_02.java to check if a number is even or odd

---
+++ Ex_02.java
@@ -0,0 +1,16 @@
+package prj_TpAula05;
+import java.util.Scanner;
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro: ");
+		int n = sc.nextInt();
+		
+		String r = (n % 2 == 0) ? "PAR" : "IMPAR";
+		System.out.println(r);
+		
+		sc.close();
+	}
+}
