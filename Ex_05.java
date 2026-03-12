3c70c7f0c29323fba2e3258cc0575ddfb6dbdce7
Francielly Santos <francielly2403@gmail.com>
2026-03-12 15:52:13 -0300

    Create Ex_05.java

---
+++ Ex_05.java
@@ -0,0 +1,28 @@
+package prj_TpAula05;
+import java.util.Scanner;
+public class Ex_05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe o código do item: ");
+		int cod = sc.nextInt();
+		System.out.println("Informe a quantidade: ");
+		int qt = sc.nextInt();
+		
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		int opcao = sc.nextInt();
+		
+		switch (opcao) {
+		case 1:
+			
+		}
+		sc.close();
+	}
+}
