a02a93189fd26979fc09e623c05d442a10950c74
Francielly Santos <francielly2403@gmail.com>
2026-03-12 17:16:07 -0300

    Create Ex_06.java with basic input handling

---
+++ Ex_06.java
@@ -0,0 +1,20 @@
+package prj_TpAula05;
+
+import java.util.Scanner;
+
+public class Ex_06 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um valor: ");
+		int num = sc.nextInt();
+		
+		if (num>=0 && num<=25) {
+		
+		}
+
+	}
+
+}
