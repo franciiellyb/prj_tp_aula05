15b23bb14f1cfeb19f01419eec58ba07920b7f23
Francielly Santos <francielly2403@gmail.com>
2026-03-12 14:53:19 -0300

    Create Ex_03.java

---
+++ Ex_03.java
@@ -0,0 +1,24 @@
+package prj_TpAula05;
+import java.util.Scanner;
+public class Ex_03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro: ");
+		int A = sc.nextInt();
+		
+		System.out.println("Informe outro número inteiro: ");
+		int B = sc.nextInt();
+		
+		if (A>B) {
+			String R = (A % B == 0) ? "Sao multiplos" : "Nao sao Multiplos";
+			System.out.println(R);
+		} else {
+			String R = (B % A == 0) ? "Sao multiplos" : "Nao sao Multiplos";
+			System.out.println(R);
+		}
+		
+		sc.close();
+	}
+}
