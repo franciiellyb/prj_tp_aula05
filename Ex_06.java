562af8af38639f1194564869a7220837a5613038
Francielly Santos <francielly2403@gmail.com>
2026-03-12 22:49:45 -0300

    Update Ex_06.java

--- Ex_06.java
+++ Ex_06.java
@@ -1,4 +1,4 @@
-package prj_TpAula05;
+package prj_tp_aula05;
 
 import java.util.Scanner;
 
@@ -9,12 +9,21 @@ public class Ex_06 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Informe um valor: ");
-		int num = sc.nextInt();
+		double num = sc.nextDouble();
 		
 		if (num>=0 && num<=25) {
-		
+			System.out.println("Intervalo [0,25]");	
+		} else if (num>25 && num<=50) {
+			System.out.println("Intervalo (25,50]");
+		} else if (num>50 && num<=75) {
+			System.out.println("Intervalo (50,75]");
+		} else if (num>75 && num<=100) {
+			System.out.println("Intervalo (75,100]");
+		} else {
+			System.out.println("Fora de Intervalo");
 		}
-
+		
+		sc.close();
 	}
-
+	
 }
