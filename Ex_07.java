29405d4ca323f16efd435087157cc670dc612779
Francielly Santos <francielly2403@gmail.com>
2026-03-12 23:56:12 -0300

    Update Ex_07.java

--- Ex_07.java
+++ Ex_07.java
@@ -7,16 +7,20 @@ public class Ex_07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Informe o 1º ponto: ");
+		System.out.println("Informe o ponto X: ");
 		double x = sc.nextDouble();
 		
-		System.out.println("Informe o 2º ponto: ");
+		System.out.println("Informe o ponto Y: ");
 		double y = sc.nextDouble();
 		
 		if (x==0 && y==0) {
 			System.out.println("Origem");
 		} else if (x<0 && y>0) {
 			System.out.println("Q2");
+		} else if (x==0 && y!=0) {
+			System.out.println("Eixo Y");
+		} else if (y==0 && x!=0) {
+			System.out.println("Eixo X");
 		} else if (x<0 && y<0) {
 			System.out.println("Q3"); 
 		}  else if(x>0 && y>0) {
@@ -24,8 +28,7 @@ public class Ex_07 {
 		} else {
 			System.out.println("Q4");
 		}
-		
 		sc.close();
 	}
-
 }
+
